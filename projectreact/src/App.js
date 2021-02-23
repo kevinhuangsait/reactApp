f80a68f7a6c9414997faab2d72dea3ef6edfe0f0
Kevin Huang <71459398+kevinhuangsait@users.noreply.github.com>
2021-02-23 02:47:33 -0700

    Initial commit

---
+++ projectreact/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Tweet from "./Tweet.js";
+
+function App() {
+  return (
+    <div>
+      <h3>Hello react</h3>
+      <Tweet />
+    </div>
+  );
+}
+
+export default App;
